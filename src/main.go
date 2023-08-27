064ebd243ad88d7bb93411c1ae63e13ca5fd7d59
ikeman32 <david.isakson.ii@gmail.com>
2023-08-27 01:40:40 -0700

    Restructure

--- src/main.go
+++ src/main.go
@@ -4,3 +4,25 @@
 // https://opensource.org/licenses/MIT
 
 package ness
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.test")
+	if err != nil {
+		panic(err)
+	}
+
+	lexer := NewLexer(file)
+	for {
+		pos, tok, lit := lexer.Lex()
+		if tok == EOF {
+			break
+		}
+
+		fmt.Printf("%d:%d\t%s\t%s\n", pos.line, pos.column, tok, lit)
+	}
+}
--- src/parser.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023 duke
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-package ness
